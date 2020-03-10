8dc457fab9218897ea71248872e8687537aa7b7e
samir19ehsani <samirehsani19@gmail.com>
2020-03-10 10:53:25 +0100

    Driver @github-Samirehsani19

--- Source/Train-Railway/Train.cs
+++ Source/Train-Railway/Train.cs
@@ -1,8 +1,25 @@
-﻿namespace Train_Railway
+﻿using System;
+namespace Train_Railway
 {
     public class Train
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
         public int Speed { get; set; }
         public int MaxSpeed { get; set; }
+        public bool Operated { get; set; }
+        public void TrainInfo(int id, string name, int speed, int maxSpeed, bool operated)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.MaxSpeed = maxSpeed;
+            this.Operated = operated;
+        }
+
+
+
+
+
+
     }
 }
