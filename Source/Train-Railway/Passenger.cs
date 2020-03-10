d268f49ea019aa554946f206cc0a6ebb59132c5d
samir19ehsani <samirehsani19@gmail.com>
2020-03-10 10:37:01 +0100

    Driver @pontusgillenang

---
+++ Source/Train-Railway/Passenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Train_Railway
+{
+	public class Passenger
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string HopOn { get; set; }
+		public string HopOff { get; set; }
+
+		public Passenger(int id, string name)
+		{
+			ID = id;
+			Name = name;
+		}
+	}
+}
