dd624b20333509691960d04df3625da43eb5a8e7
zorkii <micael.wollter@gmail.com>
2020-03-12 15:03:32 +0100

    Driver @github-aohzork

--- Source/Train-Railway/Program.cs
+++ Source/Train-Railway/Program.cs
@@ -21,15 +21,29 @@ namespace Train_Railway
         static void Main(string[] args)
         {
             IninitalizeData();
-            Thread train1 = new Thread(StartTrain);
-            train1.Start();
-            TIme();
+            List<Train> avaliableTrains = new List<Train>();
+            var tmpTrains = trains.Where(t => t.Operated == true);
+            foreach (Train train in tmpTrains)
+            {
+                avaliableTrains.Add(train);
+            }
+
+            Train train1 = avaliableTrains[0];
+            Train train2 = avaliableTrains[1];
+
+            Thread track1 = new Thread(StartTrain1);
+            Thread track2 = new Thread(StartTrain2);
+            track1.Start();
+            track2.Start();
+
+            
+            Time();
             ShowTime();
 
             Console.ReadKey();
         }
 
-        static async void TIme()
+        static async void Time()
         {
             while (true)
             {
@@ -114,7 +128,7 @@ namespace Train_Railway
             }
         }
 
-        public static void StartTrain()
+        public static void StartTrain1()
         {
             var route = tracks;
             //double distance = tracks[0].Distance*1000;
@@ -177,9 +191,72 @@ namespace Train_Railway
                 }
             }
 
+
+
             Console.WriteLine("Destination reached"); 
         }
+        public static void StartTrain2()
+        {
+            var route = tracks;
+            //double distance = tracks[0].Distance*1000;
 
+            //var startStn = tracks.Where(x => x.ID == 1).Select(x => x.StartStation).FirstOrDefault();
+            //Console.WriteLine("Start station " +  stations.Where(x => x.ID == startStn).Select(x=> x.StationName).FirstOrDefault());
 
-    }
+            //var endStn = tracks.Where(x => x.ID == 1).Select(x => x.EndStation).FirstOrDefault();
+            //Console.WriteLine("End station " + stations.Where(x => x.ID == endStn).Select(x => x.StationName).FirstOrDefault());
+
+            for (int i = 0; i < route.Count; i++)
+            {
+                double distance = tracks[i].Distance * 1000;
+                var startStn = route.Where(x => x.ID == route[i].ID).Select(x => x.StartStation).FirstOrDefault();
+                Console.WriteLine("Train starting from: " + stations.Where(x => x.ID == startStn).Select(x => x.StationName).FirstOrDefault());
+
+                var endStn = tracks.Where(x => x.ID == route[i].ID).Select(x => x.EndStation).FirstOrDefault();
+                Console.WriteLine("End station " + stations.Where(x => x.ID == endStn).Select(x => x.StationName).FirstOrDefault());
+
+                while (true)
+                {
+                    int speed = 500;
+
+                    double time = 0.5;
+
+                    Thread.Sleep(500);
+                    Console.Write(".");
+                    distance -= (speed * time);
+                    //Console.WriteLine("The distance remaining is {0}", distance);
+
+                    if (distance <= 0)
+                    {
+                        for (int y = 0; y < stations.Count; y++)
+                        {
+                            if (stations[y].ID == endStn)
+                            {
+                                stations[y].Occupied = true;
+                                //Console.WriteLine(stations[y].Occupied + " is not occupied " );
+                                if (stations[y].Occupied == false)
+                                {
+                                    stations[y].Occupied = true;
+                                    Console.WriteLine(stations[y].Occupied + " is now occupied");
+                                }
+
+                                else
+                                {
+                                    Console.WriteLine(stations[y].StationName + "is currently occupied, plz wait");
+                                    //train.wait
+                                }
+                            }
+                        }
+
+                        //Console.WriteLine("Index: " + stationIndex);
+                        //Console.WriteLine("Index: " + index);
+                        Console.WriteLine("\n\rTrain arrived at station: " + stations.Where(x => x.ID == endStn).Select(x => x.StationName).FirstOrDefault());
+                        ShowTime();
+
+                        break;
+                    }
+                }
+            }
+
+        }
 }
